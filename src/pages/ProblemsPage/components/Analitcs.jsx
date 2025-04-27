9dc2813588240532c7f199f093b46e960813cc8a
MacBook Air <macbookair@Mac.lan>
2025-04-28 00:08:51 +0500

    add

--- src/pages/ProblemsPage/components/Analitcs.jsx
+++ src/pages/ProblemsPage/components/Analitcs.jsx
@@ -16,31 +16,31 @@ const Analitcs = ({ data }) => {
             setUserId(userData.data.id);
         }
     }, [userData]);
-    const { data: statisticData, isLoading } = useGetOneQuery({
+    const { data: statisticData } = useGetOneQuery({
         key: "getAllHistoryStatistic",
         url: userId ? `/api/v1/submissions/statistics/user/${userId}` : null,
         enabled: !!userId
     })
     const easyHeight = useMemo(() => {
-        if (!data?.data?.by_difficulty?.easy || !statisticData?.data?.difficulty_distribution?.easy) return 0;
+        if (!data?.data?.by_difficulty?.easy || !statisticData?.difficulty_distribution?.easy) return 0;
         const totalEasy = data?.data?.by_difficulty?.easy;
-        const solvedEasy = statisticData?.data?.difficulty_distribution?.easy;
+        const solvedEasy = statisticData?.difficulty_distribution?.easy;
         return ((totalEasy - solvedEasy) * 206) / totalEasy;
-    }, [data?.data?.by_difficulty?.easy, statisticData?.data?.difficulty_distribution?.easy]);
+    }, [data?.data?.by_difficulty?.easy, statisticData?.difficulty_distribution?.easy]);
 
     const mediumHeight = useMemo(() => {
-        if (!data?.data?.by_difficulty?.medium || !statisticData?.data?.difficulty_distribution?.medium) return 0;
+        if (!data?.data?.by_difficulty?.medium || !statisticData?.difficulty_distribution?.medium) return 0;
         const totalMedium = data?.data?.by_difficulty?.medium;
-        const solvedMedium = statisticData?.data?.difficulty_distribution?.medium;
+        const solvedMedium = statisticData?.difficulty_distribution?.medium;
         return ((totalMedium - solvedMedium) * 206) / totalMedium;
-    }, [data?.data?.by_difficulty?.medium, statisticData?.data?.difficulty_distribution?.medium]);
+    }, [data?.data?.by_difficulty?.medium, statisticData?.difficulty_distribution?.medium]);
 
     const hardHeight = useMemo(() => {
-        if (!data?.data?.by_difficulty?.hard || !statisticData?.data?.difficulty_distribution?.hard) return 0;
+        if (!data?.data?.by_difficulty?.hard || !statisticData?.difficulty_distribution?.hard) return 0;
         const totalHard = data?.data?.by_difficulty?.hard;
-        const solvedHard = statisticData?.data?.difficulty_distribution?.hard;
+        const solvedHard = statisticData?.difficulty_distribution?.hard;
         return ((totalHard - solvedHard) * 206) / totalHard;
-    }, [data?.data?.by_difficulty?.hard, statisticData?.data?.difficulty_distribution?.hard]);  
+    }, [data?.data?.by_difficulty?.hard, statisticData?.difficulty_distribution?.hard]);  
 
     return (
         <Box p={'48px 0'}>
@@ -53,17 +53,17 @@ const Analitcs = ({ data }) => {
                             {...css.list}>{data?.data?.by_difficulty?.easy}</Box>
                         <Box
                             height={`${easyHeight}px`}
-                            bg={'#52A28A'} {...css.lists}>{statisticData?.data?.difficulty_distribution?.easy}</Box>
+                            bg={'#52A28A'} {...css.lists}>{statisticData?.difficulty_distribution?.easy}</Box>
                         <Heading color={'#52A28A'} {...css.name}>Easy</Heading>
                     </Flex>
                     <Flex flexDirection={'column'}>
                         <Box height={`${206 - mediumHeight}px`} {...css.list}>{data?.data?.by_difficulty?.medium}</Box>
-                        <Box height={`${mediumHeight}px`} bg={'#FFBF1E'} {...css.lists}>{statisticData?.data?.difficulty_distribution?.medium}</Box>
+                        <Box height={`${mediumHeight}px`} bg={'#FFBF1E'} {...css.lists}>{statisticData?.difficulty_distribution?.medium}</Box>
                         <Heading color={'#FFBF1E'} {...css.name}>Medium</Heading>
                     </Flex>
                     <Flex flexDirection={'column'}>
                         <Box height={`${206 - hardHeight}px`} {...css.list}>{data?.data?.by_difficulty?.hard}</Box>
-                        <Box height={`${hardHeight}px`} bg={'#FF6063'} {...css.lists}>{statisticData?.data?.difficulty_distribution?.hard}</Box>
+                        <Box height={`${hardHeight}px`} bg={'#FF6063'} {...css.lists}>{statisticData?.difficulty_distribution?.hard}</Box>
                         <Heading color={'#FF6063'} {...css.name}>Hard</Heading>
                     </Flex>
                 </Flex>
